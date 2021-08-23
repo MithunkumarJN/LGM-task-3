46a3dc691d11caf974682e943f1a5076fc2fd253
MithunkumarJN <87709777+MithunkumarJN@users.noreply.github.com>
2021-08-23 14:11:30 +0530

    Add files via upload

---
+++ aboutus.php
@@ -0,0 +1,32 @@
+<html>
+<head>
+    <title>About</title>
+<link rel="stylesheet" href="../csss/aboutus.css" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Flamenco" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/3.7.0/animate.min.css">
+
+</head>
+<body>
+    <header>
+      <nav>
+        <div class="row clearfix">
+          <img src="../image/logo.png" class="logo"/>
+            <ul class="main-nav" animate slideInDown>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="aboutus.php">About Us</a></li>
+                <li><a href="contactus.php">Contact Us</a></li>
+                <li><a href="../login.php">Admin Login</a></li>
+          </ul>
+        </div>
+      </nav>
+      <div class="main-content-header">
+          <h2 class="search">About Us</h2>
+             <div class="div2">
+             <h2>Introduction :</h2>
+             <p>My Name is Mithun Kumar J N From bangalore urban and now in this Present time i am live in Jalahalli <br> You can contact us mobile no 8105247479 Email id :mithunmanu007@gmail.com  <br></p>
+             </div>  
+          </div>
+    </header>
+    
+</body>
+</html>
